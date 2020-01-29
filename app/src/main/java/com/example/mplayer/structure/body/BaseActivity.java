540c02d63257975bd4928a71644d8a8a088096eb
Mihai-Cristian Popescu <mihaipopescu97@yahoo.com>
2020-01-29 16:04:17 +0200

    Added async task for base activity

--- app/src/main/java/com/example/mplayer/structure/body/BaseActivity.java
+++ app/src/main/java/com/example/mplayer/structure/body/BaseActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ import com.example.mplayer.structure.player.PlayerActivity;
 import com.example.mplayer.utils.FirebaseHandler;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,9 +34,9 @@ public class BaseActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     final FirebaseHandler firebaseHandler = FirebaseHandler.getInstance();
 
-    private String deviceId;
-    private String setupId;
-    private String roomId;
+//    private String deviceId;
+//    private String setupId;
+//    private String roomId;
     private AtomicReference<String> email = new AtomicReference<>();
     private AtomicReference<String> userId = new AtomicReference<>();
 
@@ -44,7 +46,6 @@ public class BaseActivity extends AppCompatActivity {
     private Button manageDevicesBtn;
     private Button playBtn;
 
-    private Thread thread;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -63,16 +64,17 @@ public class BaseActivity extends AppCompatActivity {
         playBtn = findViewById(R.id.playBtn);
 
         //Initially, buttons are gone
-        List<Button> list = Arrays.asList(manageDevicesBtn, managePlayliststBtn,
-                manageSetupsBtn, playBtn);
-        list.forEach(button -> button.setVisibility(View.GONE));
+//        List<Button> list = Arrays.asList(manageDevicesBtn, managePlayliststBtn,
+//                manageSetupsBtn, playBtn);
+//        list.forEach(button -> button.setVisibility(View.GONE));
+//
+//        thread = new Thread(() -> {
+//            Intent intent = getIntent();
+//            email.set(intent.getStringExtra("email"));
+//            firebaseHandler.getUserId(email.get(), userId);
+//        });
 
-        thread = new Thread(() -> {
-            Intent intent = getIntent();
-            email.set(intent.getStringExtra("email"));
-            firebaseHandler.getUserId(email.get(), userId);
-        });
-
+        new BackgroundTasks(this).execute();
 //        deviceId = getIntent().getStringExtra("deviceId");
 //        roomId = getIntent().getStringExtra("roomId");
 //        setupId = getIntent().getStringExtra("setupId");
@@ -80,30 +82,29 @@ public class BaseActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        thread.start();
-
-        while(true) {
-            if (thread.isAlive()) {
-                Log.w(TAG, "Thread not done reading");
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                Log.i(TAG, "Received email:" + email.get());
-                logOutBtn.setVisibility(View.VISIBLE);
-                manageDevicesBtn.setVisibility(View.VISIBLE);
-                break;
-            }
-        }
-    }
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        thread.start();
+//
+//        while(true) {
+//            if (thread.isAlive()) {
+//                Log.w(TAG, "Thread not done reading");
+//                try {
+//                    TimeUnit.SECONDS.sleep(1);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                Log.i(TAG, "Received email:" + email.get());
+//                logOutBtn.setVisibility(View.VISIBLE);
+//                manageDevicesBtn.setVisibility(View.VISIBLE);
+//                break;
+//            }
+//        }
+//    }
 
     public void manageDevice(View view) {
-        thread.interrupt();
         Log.i(TAG, "Sending user:" + userId.get());
         Intent intent = new Intent(BaseActivity.this, ManageDeviceActivity.class);
         intent.putExtra("userId", userId.toString());
@@ -111,29 +112,65 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     public void manageSetups(View view) {
-        thread.interrupt();
         Intent intent = new Intent(BaseActivity.this, ManageSetupActivity.class);
         intent.putExtra("deviceId", deviceId);
         startActivity(intent);
     }
 
     public void managePlaylist(View view) {
-        thread.interrupt();
         Intent intent = new Intent(BaseActivity.this, ManagePlaylistActivity.class);
         intent.putExtra("roomId", roomId);
         startActivity(intent);
     }
 
     public void play(View view) {
-        thread.interrupt();
         Intent intent = new Intent(BaseActivity.this, PlayerActivity.class);
         intent.putExtra("setupId", setupId);
         startActivity(intent);
     }
 
     public void logOutFromBase(View view) {
-        thread.interrupt();
         firebaseAuth.signOut();
         startActivity(new Intent(BaseActivity.this, MainActivity.class));
+    }
+
+    private static class BackgroundTasks extends AsyncTask<Void, Void, Void> {
+        WeakReference<BaseActivity> weakReference;
+
+        BackgroundTasks(BaseActivity activity) {
+            weakReference = new WeakReference<>(activity);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            BaseActivity activity = weakReference.get();
+
+            List<Button> list = Arrays.asList(activity.manageDevicesBtn, activity.managePlayliststBtn,
+                    activity.manageSetupsBtn, activity.playBtn);
+            list.forEach(button -> button.setVisibility(View.GONE));
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            BaseActivity activity = weakReference.get();
+            Intent intent = activity.getIntent();
+            activity.email.set(intent.getStringExtra("email"));
+            activity.firebaseHandler.getUserId(activity.email.get(), activity.userId);
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            BaseActivity activity = weakReference.get();
+            activity.manageDevicesBtn.setVisibility(View.VISIBLE);
+            activity.playBtn.setVisibility(View.VISIBLE);
+        }
     }
 }
